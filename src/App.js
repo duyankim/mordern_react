c02f03ad683689b3bd8fe160b71bde8cfc49ad97
Duyan Kim <46421950+duyankim@users.noreply.github.com>
2021-02-14 23:40:30 +0900

    custom hooks: useTabs useState를 활용한 useTabs만들기

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
 import React, { useState } from "react";
 
-const useInput = (initialValue, validator) => {
-  const [value, setValue] = useState(initialValue);
-  const onChange = (event) => {
-    const {
-      target: { value }
-    } = event;
-    let willUpdate = true;
-    if (typeof validator === "function") {
-      willUpdate = validator(value);
-    }
-    if (willUpdate) {
-      setValue(value);
-    }
+const content = [
+  {
+    tab: "Section 1",
+    content: "Content 1 from Section 1"
+  },
+  {
+    tab: "Section 2",
+    content: "Content 2 from Section 2"
+  }
+];
+
+const useTabs = (initialTab, allTabs) => {
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return;
+  }
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex
   };
-  return { value, onChange };
 };
 
 function App() {
-  const maxLen = (value) => value.length < 10;
-  const name = useInput("Mr.", maxLen);
+  const { currentItem, changeItem } = useTabs(0, content);
   return (
-    <div>
-      <h1>hello</h1>
-      <input placeholder="name" {...name} />
+    <div className="App">
+      {content.map((section, index) => (
+        <button onClick={() => changeItem(index)}>{section.tab}</button>
+      ))}
+      <div>{currentItem.content}</div>
     </div>
   );
 }
