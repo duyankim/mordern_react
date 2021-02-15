ed8721147a33c36bfe7d5779bbd3c3b12827bbd2
Duyan Kim <46421950+duyankim@users.noreply.github.com>
2021-02-15 23:58:54 +0900

    custom hooks: useFullscreen

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef, useCallback } from "react";
 
-const useScroll = () => {
-  const [state, setState] = useState({
-    x: 0,
-    y: 0
-  });
-  const onScroll = () => {
-    setState({ y: window.scrollY, x: window.scrollX });
+const useFullscreen = (callback) => {
+  const element = useRef();
+  const triggerFullscreen = () => {
+    if (element.current) {
+      element.current.requestFullscreen();
+      if (callback && typeof callback === "function") {
+        callback(true);
+      }
+    }
   };
-  useEffect(() => {
-    window.addEventListener("scroll", onScroll);
-    return () => window.removeEventListener("scroll", onScroll);
-  }, []);
-  return state;
+  const exitFullscreen = () => {
+    document.exitFullscreen();
+    if (callback && typeof callback === "function") {
+      callback(false);
+    }
+  };
+  return { element, triggerFullscreen, exitFullscreen };
 };
 
 function App() {
-  const { y } = useScroll();
+  const onFullscreen = (isFull) => {
+    console.log(isFull ? "we are full" : "we are small");
+  };
+  const { element, triggerFullscreen, exitFullscreen } = useFullscreen();
   return (
-    <div className="App" style={{ height: "19999vh" }}>
-      <h1 style={{ position: "fixed", color: y > 100 ? "red" : "blue" }}>
-        tian
-      </h1>
+    <div className="App" style={{ height: "10000vh" }}>
+      <div ref={element}>
+        <img
+          src="https://images.unsplash.com/photo-1612875895771-76bba1a61a49?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+          alt="image"
+        />
+        <button onClick={triggerFullscreen}>make fullscreen </button>
+        <button onClick={exitFullscreen}>exit fullscreen </button>
+      </div>
     </div>
   );
 }
