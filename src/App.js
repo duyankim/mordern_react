c7e63307fe276d855b146a4d8d38a64d49e2db3b
Duyan Kim <46421950+duyankim@users.noreply.github.com>
2021-02-16 00:31:19 +0900

    custom hooks: useNotification

--- src/App.js
+++ src/App.js
@@ -1,39 +1,30 @@
-import React, { useEffect, useState, useRef, useCallback } from "react";
+import React from "react";
 
-const useFullscreen = (callback) => {
-  const element = useRef();
-  const triggerFullscreen = () => {
-    if (element.current) {
-      element.current.requestFullscreen();
-      if (callback && typeof callback === "function") {
-        callback(true);
-      }
+const useNotification = (title, options) => {
+  if (!("Notification" in window)) {
+    return;
+  }
+  const fireNotif = () => {
+    if (Notification.permission !== "granted") {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          new Notification(title, options);
+        } else {
+          return;
+        }
+      });
+    } else {
+      new Notification(title, options);
     }
   };
-  const exitFullscreen = () => {
-    document.exitFullscreen();
-    if (callback && typeof callback === "function") {
-      callback(false);
-    }
-  };
-  return { element, triggerFullscreen, exitFullscreen };
+  return fireNotif;
 };
 
 function App() {
-  const onFullscreen = (isFull) => {
-    console.log(isFull ? "we are full" : "we are small");
-  };
-  const { element, triggerFullscreen, exitFullscreen } = useFullscreen();
+  const triggerNotif = useNotification("may I help you?", "description");
   return (
     <div className="App" style={{ height: "10000vh" }}>
-      <div ref={element}>
-        <img
-          src="https://images.unsplash.com/photo-1612875895771-76bba1a61a49?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
-          alt="image"
-        />
-        <button onClick={triggerFullscreen}>make fullscreen </button>
-        <button onClick={exitFullscreen}>exit fullscreen </button>
-      </div>
+      <button onClick={triggerNotif}>hello</button>
     </div>
   );
 }
---
+++ src/hooks/useFullscreen.js
@@ -0,0 +1,22 @@
+import React, { useRef } from "react";
+
+const useFullscreen = (callback) => {
+  const element = useRef();
+  const triggerFullscreen = () => {
+    if (element.current) {
+      element.current.requestFullscreen();
+      if (callback && typeof callback === "function") {
+        callback(true);
+      }
+    }
+  };
+  const exitFullscreen = () => {
+    document.exitFullscreen();
+    if (callback && typeof callback === "function") {
+      callback(false);
+    }
+  };
+  return { element, triggerFullscreen, exitFullscreen };
+};
+
+export default useFullscreen;
