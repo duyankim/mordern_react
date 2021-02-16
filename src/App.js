c31ac27e512ead569fa806f05506b8c17b47505c
Duyan Kim <46421950+duyankim@users.noreply.github.com>
2021-02-16 19:33:37 +0900

    api 연동하기

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
 import React from "react";
-import useAxios from "./hooks/useAxios";
+import Users from "./components/Users";
 
 function App() {
-  const { loading, data, error, refetch } = useAxios({
-    url: `${url}`
-  });
-  console.log(
-    `loading: ${loading}\n data:${JSON.stringify(data)} \n error: ${error}`
-  );
-  return (
-    <div className="App" style={{ height: "10000vh" }}>
-      <h1>{data && data.status}</h1>
-      <h2>{loading && "Loading"}</h2>
-      <button onClick={refetch}>refetch</button>
-    </div>
-  );
+  return <Users />;
 }
 
 export default App;
