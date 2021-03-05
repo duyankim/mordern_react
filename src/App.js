d90c3c2fd1d9492dbd75c550e9fbb752ec959c86
Duyan Kim <46421950+duyankim@users.noreply.github.com>
2021-03-05 20:06:09 +0900

    라우터 적용

--- src/App.js
+++ src/App.js
@@ -1,75 +1,11 @@
-import React, { useRef, useReducer, useMemo, useCallback } from "react";
-import UserList from "./UserList";
-import CreateUser from "./CreateUser";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom"; // * BrowserRouter 불러오기
+import App from "./App";
 
-function countActiveUsers(users) {
-  console.log("활성 사용자 수를 세는중");
-  return users.filter((user) => user.active).length;
-}
-
-const initialState = {
-  inputs: {
-    username: "",
-    email: ""
-  },
-  users: [
-    {
-      id: 1,
-      username: "velopert",
-      email: "public.velopert@gmail.com",
-      active: true
-    },
-    {
-      id: 2,
-      username: "tester",
-      email: "tester@example.com",
-      active: false
-    },
-    {
-      id: 3,
-      username: "liz",
-      email: "liz@example.com",
-      active: false
-    }
-  ]
-};
-
-function reducer(state, action) {
-  switch (action.type) {
-    case "CHANGE_INPUT":
-      return {
-        ...state,
-        inputs: {
-          ...state.inputs,
-          [action.name]: action.value
-        }
-      };
-    default:
-      return state;
-  }
-}
-
-function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const { users } = state;
-  const { username, email } = state.inputs;
-
-  const onChange = useCallback((e) => {
-    const { name, value } = e.target;
-    dispatch({
-      type: "CHANGE_INPUT",
-      name,
-      value
-    });
-  }, []);
-
-  return (
-    <>
-      <CreateUser username={username} email={email} onChange={onChange} />
-      <UserList users={users} />
-      <div>활성사용자 수 : 0</div>
-    </>
-  );
-}
-
-export default App;
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
